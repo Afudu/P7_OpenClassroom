7f7f7cdfdcf2b641de7f3971de311503f45c75e8
Afudu <abdoulseydou@hotmail.com>
2023-05-03 23:58:00 +0100

    update bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,64 +1,75 @@
 import csv
-from itertools import combinations as comb
+import itertools
 import time
 
 """
-Project brief - Part 1: create “bruteforce.py” 
-- the program needs to read a file containing information about shares,
-explore all the possible combinations, and display the best investment.
-list of limitations:
-   Each share can only be bought once.
-   We cannot buy a fraction of a share.
-   We can spend at most 500 euros per client.
+This script contains the implementation of Part1 of the Project Brief: create “bruteforce.py”.
+Details:
+    The program needs to:
+     - read a file containing information about shares,
+     - explore all the possible combinations, 
+     - and display the best investment.
+    Constraints:
+       - Each share can only be bought once.
+       - We cannot buy a fraction of a share.
+       - We can spend at most 500 euros per client.
 """
-
+# Constant variables
 MAX_COST = 500
-INITIAL_DATASET = 'data/initial_data.csv'
+DATASET = 'data/initial_dataset.csv'
 
 
 def convert_csv_to_list(csv_file):
+    """ This function reads a csv file with initially 3 columns (name, cost, and profit(%)),
+    converts its data into a list and adds a column for the return on investment."""
     shares = []
     with open(csv_file, 'r') as f:
         reader = csv.reader(f)
         next(reader)  # skip header row
         for row in reader:
-            shares.append((
-                row[0],
-                float(row[1]),
-                float(row[2])
-            ))
+            name = row[0]
+            cost = int(row[1])
+            profit = float(int(row[2]) / 100)
+            roi = profit * cost
+            shares.append({"name": name, "cost": cost, "profit": profit, "return_on_investment": roi})
     return shares
 
 
 def calculate_total_cost(share_list):
-    """calculates the sum of the costs in column of the list"""
-    total_cost = sum(s[1] for s in share_list)
+    """calculates the sum of the costs of the share_list"""
+    total_cost = sum(s['cost'] for s in share_list)
     return total_cost
 
 
 def calculate_total_return(share_list):
-    """calculates the sum of returns of the list = cost by profit"""
-    total_return_on_investment = sum(s[1] * s[2] for s in share_list)
+    """calculates the sum of returns of the share_list"""
+    total_return_on_investment = sum(s['return_on_investment'] for s in share_list)
     return total_return_on_investment
 
 
-def bruteforce_shares(csv_file, max_cost):
+def get_share_names(share_list):
+    """return the list of share names of the share_list."""
+    names = [n['name'] for n in share_list]
+    return names
+
+
+def bruteforce_shares(max_cost, dataset):
     """
     The function reads in a CSV file containing share data, loops over the list, uses
-    itertools.combinations to generate all possible combinations of shares costing less or equal to max_cost,
+    itertools.combinations to generate all possible combinations of shares less or equal to max_cost,
     then calculates the best cost, return and shares.
-    Algorithm time complexity: O(n*n!)
     """
+
+    # convert csv_file to list of key/value pairs
+    share_list = convert_csv_to_list(dataset)
+
     best_cost = 0
     best_return = 0
     best_shares = []
 
-    # convert csv_file to list of tuples
-    share_list = convert_csv_to_list(csv_file)
-
     # Try out all possible i combinations of shares
     for i in range(1, len(share_list) + 1):
-        for combination in comb(share_list, i):
+        for combination in itertools.combinations(share_list, i):
             total_cost = calculate_total_cost(combination)
             if total_cost <= max_cost:
                 total_return = calculate_total_return(combination)
@@ -72,16 +83,15 @@ def bruteforce_shares(csv_file, max_cost):
 
 # compile results & calculate execution time
 start_time = time.time()
-best_c, best_r, best_s = bruteforce_shares(INITIAL_DATASET, MAX_COST)
+best_cost_value, best_return_value, best_shares_list = bruteforce_shares(MAX_COST, DATASET)
 end_time = time.time()
 
 # display results
 print("")
 print("*********Results - Initial Dataset***********")
-print("Budget:", "€", MAX_COST)
-print(f'Best combination of shares({len(best_s)}): {[best[0] for best in best_s]}')
-print("Total cost:", "€", best_c)
-print("Total return value:", "€", round(best_r, 2))
+print(f'Budget: {MAX_COST}€')
+print(f'Best combination of shares({len(best_shares_list)}): {get_share_names(best_shares_list)}')
+print(f'Total cost: {best_cost_value}€')
+print(f'Total return value: {round(best_return_value, 2)}€')
 print("")
 print(f'Execution time: {end_time - start_time:.4f} seconds')
-# Execution time: ~ 2.2 seconds - above requirement.
